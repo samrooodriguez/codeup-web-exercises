08a9a3b01b287ef89fb4a1ae9b2111f7df97f2e7
Sam Rodriguez <sam27rodriguez@gmail.com>
2018-11-25 23:13:36 -0600

    thanksgiving break work

---
+++ js/function_drills.js
@@ -0,0 +1,58 @@
+// SIMPLE FUNCTION DRILLS ###################################################################################################
+function returnTwo() {
+    return 2;
+}
+console.log(returnTwo());
+
+function sayHowdy() {
+    return "Howdy";
+}
+console.log(sayHowdy());
+
+function returnName() {
+    return "Sam Rodriguez";
+
+}
+console.log(returnName());
+
+function addThree(num) {
+    return num + 3;
+
+}
+console.log(addThree(5));
+
+function sayString(string) {
+    return string;
+
+}
+console.log(sayString('code up'));
+//  Functions using conditionals but not loops or arrays: #################################################################################
+function isOdd(number) {
+    return "odd";
+}
+console.log(isOdd(3));
+
+function isEven(number){
+    return "even";
+}
+console.log(isEven(2));
+
+function identity(input){
+    return "identity";
+}
+console.log(identity('sam'));
+
+function isFive(input) {
+    return (input = 5);
+}
+console.log(isFive(5));
+
+function addFive(sum){
+    return sum + 5;
+}
+console.log(addFive(5));
+
+function isMultipleOfFive(input) {
+    return true;
+
+}
