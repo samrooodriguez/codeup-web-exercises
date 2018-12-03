cd86d64401f1fc8b122980deaf7bb0af38f20491
Sam Rodriguez <sam27rodriguez@gmail.com>
2018-12-03 10:46:29 -0600

    finished all weekend work

---
+++ js/objects.js
@@ -0,0 +1,165 @@
+(function () {
+    "use strict";
+
+    // /**
+    //  *
+    //  * Create an object with firstName and lastName properties that are strings
+    //  * with your first and last name. Store this object in a variable named
+    //  * `person`.
+    //  *
+    //  * Example:
+    //  *  > console.log(person.firstName) // "Rick"
+    //  *  > console.log(person.lastName) // "Sanchez"
+    //  */
+    var person = {
+        firstName: "Sam",
+        lastName: "Rodriguez",
+        fullName: function () {
+            console.log(this.firstName + " " + this.lastName)
+        },
+        sayHello: function () {
+            console.log("Hello From " + this.firstName + " " + this.lastName);
+        }
+    };
+    console.log(person.firstName); // Sam
+    console.log(person.lastName); // Rodriguez
+    person.fullName(); // Sam Rodriguez
+    person.sayHello(); // Hello from Sam Rodriguez
+    // /**
+    //  *
+    //  * Add a sayHello method to the person object that returns a greeting using
+    //  * the firstName and lastName properties.
+    //  * console.log the returned message to check your work
+    //  *
+    //  * Example
+    //  * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
+    //  */
+    // /**
+    //  If a shopper spends more than $200, they get a 12% discount.
+    //
+    //  * Write a JS program, using conditionals, that logs to the
+    //  * browser, how much Ryan, Cameron and George need to pay
+    //  *
+    //  * We know that Cameron bought $180, Ryan $250 and George $320.
+    //  *
+    //  * Your program will have to display a line with the name of the person, the amount before the
+    //  * discount, the discount, if any, and the amount after the discount.
+    //  *
+    //  * Uncomment the lines below to create an array of objects where each object
+    //  * represents one shopper. Use a foreach loop to iterate through the array,
+    //  * and console.log the relevant messages for each person
+    //  */
+        //Cameron has to pay $180;
+        // Ryan has to pay $220;
+        // George has to pay $281.60;
+    var shoppers = [
+            {
+                name: 'Cameron',
+                amount: 180
+            },
+            {
+                name: 'Ryan',
+                amount: 250
+            },
+            {
+                name: 'George',
+                amount: 320
+            }
+        ];
+    console.log(shoppers);
+
+        shoppers.forEach(function (shopper) {
+            if (shopper.amount < 200){
+                console.log(shopper.name + "  " + " still owes " + "$" +  (shopper.amount - 200) );
+            }
+            else {
+                console.log((shopper.amount * .12) - shopper.amount + "  is what " + shopper.name + " owes ");
+            }
+        });
+    // /**
+    //  * Create an array of objects that represent books and store it in a
+    //  * variable named `books`.
+    //  * Each object should have a title and an author
+    //  * property.
+    //  * The author property should be an object with properties
+    //  * `firstName` and `lastName`.
+    //  * Be creative and add at least 5 books to the
+    //  * array
+    //  *
+    //  * Example:
+    //  * > console.log(books[0].title) // "The Salmon of Doubt"
+    //  * > console.log(books[0].author.firstName) // "Douglas"
+    //  * > console.log(books[0].author.lastName) // "Adams"
+    //  */
+    var books = [
+        {
+            title: "The Alchemist",
+            author: {firstName: "Paulo", lastName: "Coelho"},
+            date: 1988
+        },
+
+        {
+            title: "And Then There Were None",
+            author: {firstName: "Agatha", lastName: "Christie"},
+            date: 1939
+        },
+
+        {
+            title: "Valley of the Dolls",
+            author: {firstName: "Jacqueline", lastName: "Susann"},
+            date: 1966
+        },
+
+        {
+            title: "Le Petit Prince",
+            author: {firstName: "Antoine de", lastName: "Saint-Exupery"},
+            date: 1943
+        },
+
+        {
+            title: "The Hobbit",
+            author: {firstName: "J.R.R", lastName: "Tolkien"},
+            date: 1937
+        }
+    ];
+    console.log(books);
+    /**
+     *
+     * Loop through the books array and output the following information about
+     * each book:
+     * - the book number (use the index of the book in the array)
+     * - the book title
+     * - author's full name (first name + last name)
+     *
+     * Example Console Output:
+     *
+     *      Book # 1
+     *      Title: The Salmon of Doubt
+     *      Author: Douglas Adams
+     *      ---
+     *      Book # 2
+     *      Title: Walkaway
+     *      Author: Cory Doctorow
+     *      ---
+     *      Book # 3
+     *      Title: A Brief History of Time
+     *      Author: Stephen Hawking
+     *      ---
+     *      ...
+     */
+    books.forEach(function (book) {
+        console.log(("Title: " + book.title) + " " +  "\n" + ("Author: "  + book.author.firstName + " " + book.author.lastName));
+    });
+    
+    /**
+     * Bonus:
+     * - Create a function named `createBook` that accepts a title and author
+     *   name and returns a book object with the properties described
+     *   previously. Refactor your code that creates the books array to instead
+     *   use your function.
+     * - Create a function named `showBookInfo` that accepts a book object and
+     *   outputs the information described above. Refactor your loop to use your
+     *   `showBookInfo` function.
+     */
+
+})();
--- js/sales_report_bonus.js
+++ js/sales_report_bonus.js
@@ -12,41 +12,59 @@
 // // ===================================================
 // // Employee Number, First Name, Last Name, Sales Units
 // // ***************************************************
-// // 1, Jane, Janeway, 3
-// // 3, Tricia, Triciason, 5
-// // 4, Jeannette, Jeanson, 4
-// // 5, Charles Emmerson III, Winchester, 2
-// // 6, Chet, Chedderson, 8
-// // 7, Chaiam, Chaiamson, 12
-// // 8, Dale, Dalesinger, 1
-// // 9, Zig, Ziglar, 50
-// // 10, Henry, Kissinger, 1
-// // 11, Arthur Herbert, Fonzarelli, 23
-// // 12, Betty, Boop, 67
+// 1, Jane, Janeway, 3
+// 3, Tricia, Triciason, 5
+// 4, Jeannette, Jeanson, 4
+// 5, Charles Emmerson III, Winchester, 2
+// 6, Chet, Chedderson, 8
+// 7, Chaiam, Chaiamson, 12
+// 8, Dale, Dalesinger, 1
+// 9, Zig, Ziglar, 50
+// 10, Henry, Kissinger, 1
+// 11, Arthur Herbert, Fonzarelli, 23
+// 12, Betty, Boop, 67
 // // "
-//T0D0 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// -create a function that converts letters into numbers - done
-// -create a function that takes in user input data and returns an array of that data - done
-// -create a function that adds (.unshift();) new data to existing array (data base)
-// *****all employee data should be an array nested within the original array******
-// -create a function that outputs all employee info with just the ID number given
-//            alert ("invalid") when invalid info is entered in
-// -create a variable (employee number) and assign it an array containing all data for given employee
-// -create a form where user can input data easily
-//        within form you should have an input for Employee Number - First Name - Last Name - Sales Units(Xus) [us = Units Sold]
-//          it should also probably include a timestamp as to WHEN each employee has been created
-// notes: will essentially be a library that contains all data that output any relevant data when called
-//               the idea behind converting letters to numbers would be some level of security
-//                -prompt user with " make sure to type in all lower case" in form
+
+// var salesReport = [1, Jane, Janeway, 3,
+//     3, Tricia, Triciason, 5,
+//     4, Jeannette, Jeanson, 4,
+//     5, Charles Emmerson III, Winchester, 2,
+//     6, Chet, Chedderson, 8,
+//     7, Chaiam, Chaiamson, 12,
+//     8, Dale, Dalesinger, 1,
+//     9, Zig, Ziglar, 50,
+//     10, Henry, Kissinger, 1,
+//     11, Arthur Herbert, Fonzarelli, 23,
+//     12, Betty, Boop, 67];
 //
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
-function charToNumber (s, i) {
-    return parseInt(s.charAt(i), 36) - 9;
+//
+// document.write("this is a test")
+var reportArray = reportContents.split('\n');
+console.log(reportArray);
+console.log(reportArray.indexOf("1, Jane, Janeway, 3"));
+var reportData = reportArray.slice(7);
+console.log(reportData);
+console.log('Number of employees is ' + reportData.length);
+var sales = 0;
+var employeeArray = [];
+for (var i = 0; i<reportData.length; i++){
+    var pick = reportData.slice(i, i+1);
+    pick = pick.toString().split(',');
+    employeeArray.push(pick);
+    pick = pick.slice(3).toString();
+    pick = Number(pick);
+    sales += pick;
 }
-function employee(a, b, c, d){
-return Array(a, b, c, d);
+console.log("Total number of units sold is " + sales);
+console.log("Average number of sales is " + sales/reportData.length);
+function sortArray (a, b){
+    return b[3]-a[3];
 }
-
+employeeArray.sort(sortArray);
+var employeeList = "";
+var employeePick = [];
+for (var k = 0; k<employeeArray.length; k++){
+    employeePick = employeeArray.slice(k, k+1).toString();
+    employeeList += employeePick+ '\n';
+}
+console.log('The list of employees, sorted by number of sales is:\n' + employeeList);;
